efc0b29e87f540d901dd5d1106b5a61de0f7ebea
Georgi Nikolov <georginikolov461@gmail.com>
2023-06-23 17:48:17 +0300

    Add files via upload

---
+++ Yard Greening/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double greeningPerimeter = double.Parse(Console.ReadLine());
+
+            double price = greeningPerimeter * 7.61;
+
+            double discount = price * 0.18;
+
+            double finalPrice = price - discount;
+
+            Console.WriteLine($"The final price is: {finalPrice:f2} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
