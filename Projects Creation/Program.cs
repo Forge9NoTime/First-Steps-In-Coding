3ec12fff285857c96b69569393ea26f8d4bb5da8
Georgi Nikolov <georginikolov461@gmail.com>
2023-06-23 17:45:53 +0300

    Add files via upload

---
+++ Projects Creation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Projects_Creation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string architectName = Console.ReadLine();
+
+            int projectsCount = int.Parse(Console.ReadLine());
+
+            int hours = projectsCount * 3;
+
+            Console.WriteLine
+                ($"The architect {architectName} will need {hours} hours to complete {projectsCount} project/s.");
+        }
+    }
+}
